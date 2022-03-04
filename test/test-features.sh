d9940fce5e33ec90a5de435515523cc40a26089b
Chuck Lantz <chuck_lantz@hotmail.com>
2022-03-04 19:56:56 +0000

    Action shim

--- test/test-features.sh
+++ test/test-features.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 test_image=${1:-"mcr.microsoft.com/vscode/devcontainers/base:ubuntu"}
+interactive="${2:-"false"}"
 devcontainer_features_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/../devcontainer-features" && pwd)"
 
-docker run -it --rm -u root -v "${devcontainer_features_dir}:/features" "${test_image}" bash /features/install.sh true
+if [ "${interactive}" == "true" ]; then
+    docker run -it --rm -u root -v "${devcontainer_features_dir}:/features" "${test_image}" bash
+else 
+    docker run -it --rm -u root -v "${devcontainer_features_dir}:/features" "${test_image}" bash /features/install.sh true
+fi
